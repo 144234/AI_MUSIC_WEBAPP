e39a60a8b8e3ba63832733e823b7a67c4eaa7ad9
144234 <78003838+144234@users.noreply.github.com>
2023-12-03 13:31:05 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,18 +1,46 @@
 song1 = "";
 song2= "";
 
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristy = 0;
+
 function preload(){
-    song1 = loadSound("Taylor Swift - Style .mp3")
-    song2 = loadSound("Taylor Swift - Getaway Car .mp3")
+    song1 = loadSound("TaylorSwift_Style.mp3")
+    song2 = loadSound("TaylorSwift_GetawayCar.mp3")
 }
 
 function setup(){
     canvas = createCanvas(500,600);
-    canvas.Center();
-    video=createCapture(VIDEO);
-    video.hide();
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide()
+    poseNet=ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose',gotPoses);
+ 
 }
 
 function draw(){
     image(video, 0, 0, 500, 600);
+}
+
+function modelLoaded(){
+    console.log('PoseNet is Initialized');
+ }
+ 
+ unction gotPoses(results)
+{
+   if(results.length > 0)
+   {
+       console.log(results);
+       leftWristX = results[0].pose.leftWrist.x;
+       console.log('Left Wrist X = ' + leftWristX);
+       leftWristY = results[0].pose.leftWrist.y;
+       console.log('Left Wrist Y = ' + leftWristY);
+       rightWristX = results[0].pose.rightWrist.x;
+       console.log('Right Wrist X = ' + rightWristX);
+       rightWristY = results[0].pose.rightWrist.y;
+       console.log('Right Wrist Y = ' + rightWristY);
+   }
 }
